4ad4fe56b1d48b8de7cd7c2500df1af0f9e7e563
echoisonline <chozachel13@gmail.com>
2025-03-10 19:05:46 +0300

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import "../src/index.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500">hello</h1>
+    </>
+  );
+}
+
+export default App;
